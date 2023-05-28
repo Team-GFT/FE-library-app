43205490cb1647d435849253df696c13d06990cf
kmonguu <skdus8009@gmail.com>
2023-05-28 18:49:42 +0900

    feat: sidebar에 icon 추가 및 스타일 수정

--- src/components/Layout/Sidebar/SidebarItem.tsx
+++ src/components/Layout/Sidebar/SidebarItem.tsx
@@ -1,22 +1,37 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { theme } from "../../../styles/theme";
+import { ReactNode } from "react";
 
 interface SidebarItemProps {
-  menu: { name: string; path: string };
+  menu: { name: string; path: string; icon: ReactNode };
 }
 
 function SidebarItem({ menu }: SidebarItemProps) {
   return (
     <div css={sidebarItemCss}>
+      {menu.icon}
       <p>{menu.name}</p>
     </div>
   );
 }
 
 const sidebarItemCss = css`
-  display: inline-block;
+  display: flex;
   text-align: left;
   text-decoration: none;
-  color: #000;
+  color: ${theme.keyColor};
+  font-size: 24px;
+  font-weight: bold;
+  gap: 20px;
+
+  :hover {
+    color: ${theme.keyDark};
+    cursor: pointer;
+
+    svg > g {
+      fill: ${theme.keyDark};
+    }
+  }
 `;
 export default SidebarItem;
--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -2,32 +2,76 @@
 import { css } from "@emotion/react";
 import { Link } from "react-router-dom";
 import SidebarItem from "./SidebarItem";
+import { useState } from "react";
+import { ReactComponent as More } from "/src/assets/More.svg";
+import { ReactComponent as BookList } from "/src/assets/BookList.svg";
+import { ReactComponent as Member } from "/src/assets/Member.svg";
+import { ReactComponent as History } from "/src/assets/History.svg";
 
 function Sidebar() {
   const Menus = [
-    { name: "도서 목록", path: "/" },
-    { name: "회원 관리", path: "/members" },
-    { name: "대출 이력", path: "/history" },
+    { name: "도서 목록", path: "/", icon: <BookList /> },
+    { name: "회원 관리", path: "/members", icon: <Member /> },
+    { name: "대출 이력", path: "/history", icon: <History /> },
   ];
+  const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+  const handleSidebar = () => {
+    setIsOpenSidebar((prev) => !prev);
+  };
 
   return (
-    <div css={sidebarCss}>
-      {Menus.map((menu, index) => (
-        <Link to={menu.path} key={index}>
-          <SidebarItem menu={menu} />
-        </Link>
-      ))}
-    </div>
+    <nav
+      css={sidebarCss}
+      className={isOpenSidebar ? "open_sidebar" : "close_sidebar"}
+      onMouseEnter={handleSidebar}
+      onMouseLeave={handleSidebar}
+    >
+      {isOpenSidebar ? (
+        <>
+          {Menus.map((menu, index) => (
+            <Link to={menu.path} key={index}>
+              <SidebarItem menu={menu} />
+            </Link>
+          ))}
+        </>
+      ) : (
+        <>
+          <More />
+          <div>
+            {Menus.map((menu) => (
+              <>{menu.icon}</>
+            ))}
+          </div>
+        </>
+      )}
+    </nav>
   );
 }
 
 const sidebarCss = css`
   display: flex;
   flex-direction: column;
-  flex: 1;
   height: 100%;
-  width: 100px;
+  width: 30px;
   box-shadow: rgba(149, 157, 165, 0.1) 0px 8px 24px;
+  transition: all 0.2s;
+
+  &.open_sidebar {
+    width: 13rem;
+    padding: 85px 50px;
+    gap: 2rem;
+  }
+
+  &.close_sidebar {
+    padding: 30px;
+
+    div {
+      display: flex;
+      flex-direction: column;
+      margin-top: 30px;
+      gap: 20px;
+    }
+  }
 `;
 
 export default Sidebar;
