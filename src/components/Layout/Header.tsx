0afe2b0f553e58b45371d40e3ac68c868325d2fd
kmonguu <skdus8009@gmail.com>
2023-05-29 20:13:24 +0900

    feat: Layout Header에 버튼 추가 작업

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,20 +1,89 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { ReactComponent as Logo } from "/src/assets/Logo.svg";
+import { ReactNode, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { theme } from "../../styles/theme";
+import Modal from "../../common/Modal";
 
-function Header() {
+type pageType = {
+  name: string;
+  headerButtonText?: string;
+  path: string;
+  icon: ReactNode;
+};
+
+interface HeaderProps {
+  pages: {
+    name: string;
+    headerButtonText?: string;
+    path: string;
+    icon: ReactNode;
+  }[];
+}
+
+function Header({ pages }: HeaderProps) {
+  const [currentPageInfo, setCurrentPageInfo] = useState<pageType>();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+    const findPath = pages.find((page) => page.path === path);
+    if (!findPath) return;
+
+    setCurrentPageInfo(findPath);
+  }, [location]);
+
   return (
-    <header css={headerCss}>
-      <Logo />
-    </header>
+    <>
+      {currentPageInfo && (
+        <header css={headerCss}>
+          {currentPageInfo.name}
+          <button
+            type="button"
+            css={bookAddButtonCss}
+            onClick={handleModalOpen}
+          >
+            {currentPageInfo?.headerButtonText}
+          </button>
+        </header>
+      )}
+      {isModalOpen && <Modal title={currentPageInfo?.headerButtonText} />}
+    </>
   );
 }
 
 const headerCss = css`
-  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 95%;
   height: 80px;
-  padding: 0 40px;
+  padding: 0 50px;
   box-shadow: rgba(149, 157, 165, 0.1) 0px 8px 24px;
+
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const bookAddButtonCss = css`
+  border: none;
+  background: ${theme.keyColor};
+  padding: 13px 30px;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 16px;
+  color: ${theme.background};
+
+  :hover {
+    background: ${theme.keyLight};
+    cursor: pointer;
+  }
 `;
 
 export default Header;
